3ca52c3b35fc51a9bb7391fecedb2bd6be672da3
Hisham Bin Seddeq <prog.hbs@hotmail.com>
2019-10-01 16:44:19 +0300

    added functions exercises file

---
+++ FunctionExercises.js
@@ -0,0 +1,34 @@
+function youRock(name) {
+
+    console.log("you rock "+name);
+    
+}
+youRock("Hisham");
+
+
+function square(num) {
+
+    console.log(num*num);
+    
+}
+
+square(3);
+
+function cube(num) {
+
+
+    console.log(num*num*num);
+    
+}
+
+cube(2);
+
+function toTheFourth(num) {
+
+
+    console.log(num*num*num*num);
+    
+}
+
+toTheFourth(2);
+
--- FunctionsObjects.js
+++ FunctionsObjects.js
@@ -48,3 +48,5 @@ let bankAccount = (function(){
 
   
   
+
+  
